e23c6c80cf976db80cbb5ba5a5e22fc5ae3947ab
Albert T <54173512+hunteraf87@users.noreply.github.com>
2023-01-07 19:34:05 +0300

    Удаление директории компиляции

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThreadsManager = void 0;
-var threads_manager_1 = require("./threads-manager");
-Object.defineProperty(exports, "ThreadsManager", { enumerable: true, get: function () { return threads_manager_1.ThreadsManager; } });
--- dist/threads-manager/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ThreadsManager = void 0;
-var threads_manager_1 = require("./threads-manager");
-Object.defineProperty(exports, "ThreadsManager", { enumerable: true, get: function () { return __importDefault(threads_manager_1).default; } });
